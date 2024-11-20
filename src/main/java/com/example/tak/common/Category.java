d159c3e4723921bc806a0a9fff3a4d81b90cf05f
jgmoon <6813821@tukorea.ac.kr>
2024-11-20 10:49:21 +0900

    feat(#11): Nation 추가
    한국, 미국

--- src/main/java/com/example/tak/common/Category.java
+++ src/main/java/com/example/tak/common/Category.java
@@ -9,9 +9,7 @@ public enum Category {
     GROWTH("성장", 1),
     LEVERAGE("레버리지", 2),
     DIVIDEND("배당", 3),
-    GOLD("금", 4),
-    KOREA("한국", 5),
-    US("미국", 6);
+    GOLD("금", 4);
 
     private final String description;
     private final int code;
