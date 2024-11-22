1350156136607b113da12b05b3a66de83e146f32
jgmoon <6813821@tukorea.ac.kr>
2024-11-22 15:37:35 +0900

    feat(#17): Category, Nation 컨버터 응답 처리

--- src/main/java/com/example/tak/common/Category.java
+++ src/main/java/com/example/tak/common/Category.java
@@ -1,16 +1,32 @@
 package com.example.tak.common;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
 public enum Category {
-    GROWTH("성장", 1),
-    LEVERAGE("레버리지", 2),
-    DIVIDEND("배당", 3),
-    GOLD("금", 4);
+    GROWTH("성장"),
+    LEVERAGE("레버리지"),
+    DIVIDEND("배당"),
+    GOLD("금");
 
     private final String description;
-    private final int code;
+
+    @JsonCreator
+    public static Category fromName(String name) {
+        for (Category category : Category.values()) {
+            if (category.name().equalsIgnoreCase(name)) {
+                return category;
+            }
+        }
+        throw new IllegalArgumentException("Invalid Category name: " + name);
+    }
+
+    @JsonValue
+    public String getName() {
+        return this.name();
+    }
 }
