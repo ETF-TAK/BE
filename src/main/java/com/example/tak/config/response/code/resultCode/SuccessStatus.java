f4360da0863789fb8d8ddf08a21b75932db2293d
suyong <edgarlee9364@gmail.com>
2024-11-22 13:18:10 +0900

    feat: 응답 성공 결과 코드 구현

---
+++ src/main/java/com/example/tak/config/response/code/resultCode/SuccessStatus.java
@@ -0,0 +1,40 @@
+package com.example.tak.config.response.code.resultCode;
+
+import com.example.tak.config.response.code.BaseCode;
+import com.example.tak.config.response.code.ReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다."),
+
+    // ETF 관련 성공 응답
+    ETF_SEARCH_SUCCESS(HttpStatus.OK, "ETF2001", "ETF 검색 성공");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDto getReason() {
+        return ReasonDto.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .build();
+    }
+
+    @Override
+    public ReasonDto getReasonHttpStatus() {
+        return ReasonDto.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .build();
+    }
+}
