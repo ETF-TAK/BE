547712ee55adf06185e38ba6448b3e996694e6d7
suyong <edgarlee9364@gmail.com>
2024-12-04 08:26:34 +0900

    fix(#46): ETF 비교 화면 응답값 추가 (ticker, etfNum, secotr)

--- src/main/java/com/example/tak/dto/response/EtfInfoResponse.java
+++ src/main/java/com/example/tak/dto/response/EtfInfoResponse.java
@@ -11,6 +11,7 @@ public class EtfInfoResponse {
 
     private String name;
     private String company;
+    private String sector;
     private String listingDate; // 상장일
     private Long netWorth; // 순자산
     private Float dividendRate; // 연간 배당률
